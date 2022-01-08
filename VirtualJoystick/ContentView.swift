0c9a8e81a0cd987df87ffdf6666e08c182590d87
Luke Lazzaro <zodiac44@hotmail.com>
2022-01-08 17:24:50 +1100

    Initial Commit

---
+++ VirtualJoystick/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VirtualJoystick
+//
+//  Created by Luke Lazzaro on 8/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VirtualJoystick/VirtualJoystickApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualJoystickApp.swift
+//  VirtualJoystick
+//
+//  Created by Luke Lazzaro on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct VirtualJoystickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
